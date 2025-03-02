1d1d2f232c0dd707ce970b0ff0b279401d695ac8
tgyuzelev <tgyuzelev@gmail.com>
2025-03-02 11:55:51 +0200

    Print book report

--- main.py
+++ main.py
@@ -4,11 +4,33 @@ def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
 
+def print_book_report(book_file_path, words_count, characters_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+
+    for char_data in characters_count:
+        symbol = char_data["symbol"]
+
+        if symbol.isalpha() == False:
+            continue
+        
+        count = char_data["count"]
+        print(f"{symbol}: {count}")
+
+    print("============= END ===============")
+
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_file_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+
     words_count = count_words(book_text)
-    print(f"{words_count} words found in the document")
+
     data = count_characters(book_text)
-    print(sort_characters_count(data))
+    sorted_data = sort_characters_count(data)
+
+    print_book_report(book_file_path, words_count, sorted_data)
 
 main()
